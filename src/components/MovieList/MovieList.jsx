e3de7e5fc09d0fad0c7dd87499f6675a80b487ed
PavloMolytovnyk <pavlomolytovny@icloud.com>
2026-05-08 20:49:21 +0300

    Change

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,9 @@
 import './MovieList.scss';
+import movies from "../../api/movies.json";
+import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = ({ movies }) => (
+  <div className="movies">
+    {movies.map(film => (<MovieCard key={film.imdbId} movie={film}/>) )}
+  </div>
+);
